908b86dca79b21b023670d4250d22ad956265426
Cliff Lee CL <tommyboys0107@gmail.com>
2021-09-27 01:41:21 +0800

    Change TennisKana0.sln to TennisKana.sln and add TennisKana001 unit test project.

---
+++ TennisKana001/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TennisKana001
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
