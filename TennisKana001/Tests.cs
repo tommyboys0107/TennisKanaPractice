10e4e6ce3c04024b41acbe9ef6dbcf7c2f5c6235
Cliff Lee CL <tommyboys0107@gmail.com>
2021-10-03 20:15:42 +0800

    Tennis kana 001 Practice.

--- TennisKana001/Tests.cs
+++ TennisKana001/Tests.cs
@@ -3,12 +3,47 @@
 namespace TennisKana001
 {
     [TestFixture]
-    public class Tests
+    public class TennisTests
     {
-        [Test]
-        public void Test1()
+        private Tennis _tennis;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.True(true);
+            _tennis = new Tennis();
+        }
+        
+        [Test]
+        public void LoveAll()
+        {
+            ScoreShouldBe("Love All");
+        }
+        
+        [Test]
+        public void FifteenLove()
+        {
+            GivenFirstPlayerScore(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+        
+        [Test]
+        public void ThirtyLove()
+        {
+            GivenFirstPlayerScore(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        private void GivenFirstPlayerScore(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.FirstPlayerScore();
+            }
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennis.Score());
         }
     }
 }
