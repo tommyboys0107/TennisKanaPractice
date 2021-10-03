404b06be63142325572d9b98a30279a683c774b8
Cliff Lee CL <tommyboys0107@gmail.com>
2021-10-03 21:48:32 +0800

    Tennis kana 004 Practice. (still old style) #0

---
+++ TennisKana004/Tennis.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace TennisKana004
+{
+    public class Tennis
+    {
+        private int _firstPlayerScoreTimes;
+
+        private readonly Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {1, "Fifteen"},
+            {2, "Thirty"}
+        };
+
+        public string Score()
+        {
+            if (_firstPlayerScoreTimes > 0)
+            {
+                return $"{_scoreLookup[_firstPlayerScoreTimes]} Love";
+            }
+
+            return "Love All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScoreTimes++;
+        }
+    }
+}
---
+++ TennisKana004/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TennisKana004
+{
+    [TestFixture]
+    public class TennisTest
+    {
+        private Tennis _tennis;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tennis = new Tennis();
+        }
+        
+        [Test]
+        public void LoveAll()
+        {
+            ScoreShouldBe("Love All");
+        }
+        
+        [Test]
+        public void FifteenLove()
+        {
+            GivenFirstPlayerScore(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+        
+        [Test]
+        public void ThirtyLove()
+        {
+            GivenFirstPlayerScore(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        private void GivenFirstPlayerScore(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.FirstPlayerScore();
+            }
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennis.Score());
+        }
+    }
+}
