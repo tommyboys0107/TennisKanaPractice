19a1ae43e86385647a6e37cdce9cca7648d24144
Cliff Lee CL <tommyboys0107@gmail.com>
2021-10-17 23:14:59 +0800

    Tennis Kana #2 (Vim)

---
+++ TennisKana002/Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace TennisKana002
+{
+    [TestFixture]
+    public class TennisTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _tennis = new Tennis();
+        }
+
+        private Tennis _tennis;
+
+
+        [Test]
+        public void LoveAll()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [Test]
+        public void FifteenLove()
+        {
+            GivenFirstPlayerScore(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [Test]
+        public void ThirtyLove()
+        {
+            GivenFirstPlayerScore(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        private void GivenFirstPlayerScore(int times)
+        {
+            for (var i = 0; i < times; i++) _tennis.FirstPlayerScore();
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennis.Score());
+        }
+    }
+}
